9f679509883fb614e67920edb3a57761fbc67ab3
José Augusto Martins Nieviadonski <jose.martins@opet.edu.br>
2017-02-23 12:08:09 -0300

    Versão 2.0

--- ControleBiblioteca/src/br/edu/opet/biblioteca/dto/AlunoDTO.java
+++ ControleBiblioteca/src/br/edu/opet/biblioteca/dto/AlunoDTO.java
@@ -1,5 +1,28 @@
 package br.edu.opet.biblioteca.dto;
 
-public class AlunoDTO {
+import java.util.List;
 
+import br.edu.opet.biblioteca.model.Aluno;
+
+public class AlunoDTO extends AbstractDTO<Aluno>
+{
+    public AlunoDTO(boolean pOk, String pMensagem)
+    {
+        super(pOk, pMensagem);
+    }
+
+    public AlunoDTO(boolean pOk, String pMensagem, Aluno pObjeto)
+    {
+        super(pOk, pMensagem, pObjeto);
+    }
+
+    public AlunoDTO(boolean pOk, String pMensagem, List<Aluno> pLista)
+    {
+        super(pOk, pMensagem, pLista);
+    }
+
+    public Aluno getALuno()
+    {
+        return getObjeto();
+    }
 }
