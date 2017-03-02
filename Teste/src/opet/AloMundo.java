9c6e49f0cc39b1458cdfd0782b8b6f3ecb3a50ba
josecwb <jose.martins@opet.edu.br>
2017-03-02 20:59:39 -0300

    Update AloMundo.java

--- Teste/src/opet/AloMundo.java
+++ Teste/src/opet/AloMundo.java
@@ -5,7 +5,8 @@ public class AloMundo
 
     public static void main(String[] args)
     {
-        System.out.println("Alo mundo");
+        System.out.println("Alo mundo cruel");
+        // Teste
         // Teste
 
     }
