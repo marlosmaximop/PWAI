cf9f23f807a3a680aae54a165ac6481128384887
aluno <aluno@172.31.1.215>
2017-03-02 10:52:54 -0300

    teste

--- teste/src/opet/view/Alo.java
+++ teste/src/opet/view/Alo.java
@@ -3,8 +3,7 @@ package opet.view;
 public class Alo {
 
 	public static void main(String[] args) {
-		System.out.println("alo mundoS");
-
+		
 	}
 
 }
--- teste/src/opet/view/Alo2.java
+++ teste/src/opet/view/Alo2.java
@@ -2,5 +2,6 @@ package opet.view;
 
 public class Alo2 {
  public static void main(String[] args) {
+	 
 }
 }
